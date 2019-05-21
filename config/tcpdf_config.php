25e82aef3db83d12c8d751bdc1f3513e08877cb5
Stephen Wolff <stephen@maxgatedigital.com>
2019-05-21 13:31:11 +0100

    Arlico config edit (TCPDF could do with local settings)

--- config/tcpdf_config.php
+++ config/tcpdf_config.php
@@ -45,13 +45,13 @@
  * Installation path (/var/www/tcpdf/).
  * By default it is automatically calculated but you can also set it as a fixed string to improve performances.
  */
-//define ('K_PATH_MAIN', '');
+// define ('K_PATH_MAIN', '');
 
 /**
  * URL path to tcpdf installation folder (http://localhost/tcpdf/).
  * By default it is automatically set but you can also set it as a fixed string to improve performances.
  */
-//define ('K_PATH_URL', '');
+// define ('K_PATH_URL', '');
 
 /**
  * Path for PDF fonts.
@@ -69,12 +69,12 @@
  * Deafult image logo used be the default Header() method.
  * Please set here your own logo or an empty string to disable it.
  */
-//define ('PDF_HEADER_LOGO', '');
+define ('PDF_HEADER_LOGO', 'files/site/logo.png');
 
 /**
  * Header logo image width in user units.
  */
-//define ('PDF_HEADER_LOGO_WIDTH', 0);
+define ('PDF_HEADER_LOGO_WIDTH', 50);
 
 /**
  * Cache directory for temporary files (full path).
@@ -104,7 +104,7 @@ define ('PDF_CREATOR', 'TCPDF');
 /**
  * Document author.
  */
-define ('PDF_AUTHOR', 'TCPDF');
+define ('PDF_AUTHOR', 'Arlico');
 
 /**
  * Header title.
@@ -114,7 +114,7 @@ define ('PDF_HEADER_TITLE', 'TCPDF Example');
 /**
  * Header description string.
  */
-define ('PDF_HEADER_STRING', "by Nicola Asuni - Tecnick.com\nwww.tcpdf.org");
+define ('PDF_HEADER_STRING', "by Ed Wain - www.arlico.co.uk");
 
 /**
  * Document unit of measure [pt=point, mm=millimeter, cm=centimeter, in=inch].
@@ -124,37 +124,43 @@ define ('PDF_UNIT', 'mm');
 /**
  * Header margin.
  */
-define ('PDF_MARGIN_HEADER', 5);
+// define ('PDF_MARGIN_HEADER', 5);
+define ('PDF_MARGIN_HEADER', 0);
 
 /**
  * Footer margin.
  */
-define ('PDF_MARGIN_FOOTER', 10);
+// define ('PDF_MARGIN_FOOTER', 10);
+define ('PDF_MARGIN_FOOTER', 0);
 
 /**
  * Top margin.
  */
-define ('PDF_MARGIN_TOP', 27);
+// define ('PDF_MARGIN_TOP', 27);
+define ('PDF_MARGIN_TOP', 0);
 
 /**
  * Bottom margin.
  */
-define ('PDF_MARGIN_BOTTOM', 25);
+// define ('PDF_MARGIN_BOTTOM', 25);
+define ('PDF_MARGIN_BOTTOM', 20);
 
 /**
  * Left margin.
  */
-define ('PDF_MARGIN_LEFT', 15);
+// define ('PDF_MARGIN_LEFT', 15);
+define ('PDF_MARGIN_LEFT', 0);
 
 /**
  * Right margin.
  */
-define ('PDF_MARGIN_RIGHT', 15);
+// define ('PDF_MARGIN_RIGHT', 15);
+define ('PDF_MARGIN_RIGHT', 0);
 
 /**
  * Default main font name.
  */
-define ('PDF_FONT_NAME_MAIN', 'helvetica');
+define ('PDF_FONT_NAME_MAIN', 'LatoLight');
 
 /**
  * Default main font size.
@@ -164,7 +170,7 @@ define ('PDF_FONT_SIZE_MAIN', 10);
 /**
  * Default data font name.
  */
-define ('PDF_FONT_NAME_DATA', 'helvetica');
+define ('PDF_FONT_NAME_DATA', 'LatoLight');
 
 /**
  * Default data font size.
