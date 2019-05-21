dcb86a6224d7dcc3ba4f22c6a610a2b429edf890
Stephen Wolff <stephen@maxgatedigital.com>
2019-05-21 13:30:54 +0100

    Arlico parameters notes

---
+++ parameters.php
@@ -0,0 +1,6 @@
+<?php
+/*
+    TCPDF::Image         ( $file, $x, $y, $w, $h, $type='', $link='', $align='', $resize=false, $dpi=300, $palign='', $ismask=false, $imgmask=false, $border=0, $fitbox=false, $hidden=false, $fitonpage=false, $alt=false, $altimgs=array() );
+    TCPDF::MultiCell     ( $w, $h, $txt, $border=0, $align='J', $fill=false, $ln=1, $x, $y, $reseth=true, $stretch=0, $ishtml=false, $autopadding=true, $maxh=0, $valign='T', $fitcell=false );
+    TCPDF::WriteHTMLCell ( $w, $h, $x, $y, $html, $border=0, $ln=0, $fill=false, $reseth=true, $align='', $autopadding=true );
+*/
